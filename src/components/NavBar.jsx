76c14c32543ffa422df6c88524a998a20e0d64d7
Santiago <santicg01@gmail.com>
2025-03-20 14:04:07 -0300

    ProyectoFinal+Castañares

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { NavLink } from 'react-router';
 import CartWidget from './CartWidget';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,11 +10,11 @@ function NavBar () {
     <>
       <Navbar bg='dark' data-bs-theme='dark'>
         <Container>
-          <Navbar.Brand as={Link} to='/'> Dimensional Art</Navbar.Brand>
+          <Navbar.Brand as={NavLink} to='/'> Dimensional Art</Navbar.Brand>
           <Nav className='me-auto'>
-            <Nav.Link as={Link} to="/category/smartphones">Smartphone</Nav.Link>
-            <Nav.Link as={Link} to="/category/laptops">Laptops</Nav.Link>
-            <Nav.Link as={Link} to="/category/fragrances">Fragrances</Nav.Link>
+            <Nav.Link as={NavLink} to="/category/smartphones">Smartphone</Nav.Link>
+            <Nav.Link as={NavLink} to="/category/laptops">Laptops</Nav.Link>
+            <Nav.Link as={NavLink} to="/category/fragrances">Fragrances</Nav.Link>
           </Nav>
           <CartWidget/>
         </Container>
