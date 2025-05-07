06bed7a79481887f1c6f11ec277b1c5a4e1e4158
Santiago <santicg01@gmail.com>
2025-05-07 18:12:40 -0300

    proximamente

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<ItemListContainer/>} />
           <Route path="/category/:id" element={<ItemListContainer/>} />
+          <Route path="/category/sublimaciones" element={<p>Proximamente</p>} />
           <Route path="/item/:id" element={<ItemDetailContainer/>} />
           <Route path="/cart" element={<CartContainer/>} />
           <Route path="/checkout" element={<CheckoutForm/>} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,10 +12,8 @@ function NavBar () {
         <Container>
           <Navbar.Brand as={NavLink} to='/'> Dimensional Art</Navbar.Brand>
           <Nav className='me-auto'>
-            <Nav.Link as={NavLink} to="/category/smartphones">Smartphone</Nav.Link>
-            <Nav.Link as={NavLink} to="/category/laptops">Laptops</Nav.Link>
-            <Nav.Link as={NavLink} to="/category/fragrances">Fragrances</Nav.Link>
             <Nav.Link as={NavLink} to="/category/impreciones">Impreciones 3D</Nav.Link>
+            <Nav.Link as={NavLink} to="/category/sublimaciones">Sublimaciones</Nav.Link>
           </Nav>
           <CartWidget/>
         </Container>
